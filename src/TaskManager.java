78471bb14ec0b6dbdb77157e10706de54deda3a9
K-0x4b <kareem.m.dev@gmail.com>
2025-06-26 15:42:57 +0000

    Made the TaskManager class

---
+++ src/TaskManager.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+
+public class TaskManager {
+
+    public ArrayList<Task> tasks;
+    public int nextId;
+
+   public TaskManager() {
+        tasks = new ArrayList<>();
+        nextId = 1; // Start IDs from 1
+    }
+
+    public void addTask(String title, String description, String dueDate, int priority) {
+        Task task = new Task(nextId, title, description, dueDate, priority);
+        nextId++;
+        tasks.add(task);
+    }
+
+    public void listTasks() {
+        if (task.isEmpty()) {
+            System.out.println("No tasks available.");
+            return;
+        }
+            for (Task task : tasks) {
+                System.out.println(task.toString());
+        }
+       
+    }
+
+    public boolean markTaskCompleted(int id) {
+        for (Task task : tasks) {
+            if (task.getId == id) {
+                task.setCompleted(true);
+                return true;
+            }
+        }
+        return false; 
+    }
+
+    public boolean delteTask(int id) {
+        for (Task task : tasks) {
+            if (task.getId == id) {
+                tasks.remove(task);
+                return true;
+            }
+        }
+        return false; 
+    }
+
+    public Task findTaskById(int id) {
+        for (Task task : tasks) {
+            if (task.getId == id) {
+                return task;
+            }
+        }
+        return null; 
+    }
+
+    
+}
